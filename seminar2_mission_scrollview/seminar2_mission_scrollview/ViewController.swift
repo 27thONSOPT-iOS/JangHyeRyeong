cd81d70a194f431201151bf1001de0dc49f92234
hryeong66 <jhr110326@gmail.com>
2020-10-30 23:17:20 +0900

    seminar2_misson_scrollview

---
+++ seminar2_mission_scrollview/seminar2_mission_scrollview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar2_mission_scrollview
+//
+//  Created by 장혜령 on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
